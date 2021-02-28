99af7a1526533c965e0334878a4fca2da7cc0a79
abdelilahelhaddad <elhaddadabdelilah@gmail.com>
2021-02-28 18:05:17 +0100

    :fire: :art: Fourth Commit Desktop Layout :sparkles: :globe_with_meridians:

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const price = document.querySelector(".price");
 let pageViewsVal = ['10K', '50K', '100K', '500K', '1M'];
 let priceVal = ['$8.00', '$12.00', '$16.00', '$24.00', '$36.00'];
 
+// range.oninput = ()=>{
+//   price.value
+// }
 
-range.addEventListener("oninput", () => {
+range.addEventListener("mousemove", () => {
   let sliderValue = range.value;
+  pageviews.innerHTML = pageViewsVal[sliderValue];
+  price.innerHTML = priceVal[sliderValue];
   let sliderColor = 'linear-gradient(90deg, $Full-Slider-Bar' + sliderValue + '%, $Empty-Slider-Bar' + sliderValue +'%)';
   range.style.background = sliderColor;
 });
