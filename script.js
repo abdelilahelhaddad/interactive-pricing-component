4306d851484ba7947bcf9b830e0b7e56859095e2
abdelilahelhaddad <elhaddadabdelilah@gmail.com>
2021-03-02 13:29:00 +0100

    :iphone: :seedling: Mobile Responsive 375px :sparkles: :iphone:

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const range = document.querySelector(".slider");
 const pageviews = document.querySelector(".pageviews");
 const price = document.querySelector(".price");
 const plan = document.querySelector("#plan");
+const pv = document.querySelector(".pv>p");
 
 const toggle = document.querySelector(".toggle-icon");
 const circle = document.querySelector(".circle");
@@ -10,22 +11,34 @@ let pageViewsVal = ['10', '50', '100', '500', '1000'];
 let priceVal = ['8.00', '12.00', '16.00', '24.00', '36.00'];
 
 range.addEventListener("mousemove", () => {
+  //Get range slider value [0,1,2,3,4]
   let sliderValue = range.value;
+  //Change range background color when sliding
+  let sliderColor = `linear-gradient(90deg, rgb(165,243,235) ${(sliderValue * 25)}%, rgb(234,238,251) ${(sliderValue * 25)}%)`;
+  range.style.background = sliderColor;
+
   if (toggle.classList.contains("active")) {
     plan.innerHTML = "/year";
-    pageviews.innerHTML = ((pageViewsVal[sliderValue]) * 12);
-    pVal = (priceVal[sliderValue] * 12 - (25/100));
-    price.innerHTML = Math.floor(pVal);
+    pvVal = ((pageViewsVal[sliderValue]) * 12);
+    pageviews.innerHTML = pvVal;
+    prVal = ((priceVal[sliderValue] * 12) - (25/100));
+    price.innerHTML = Math.floor(prVal);
+    // if (pvVal >=1000 ) {
+    //   pageviews.innerHTML = pvVal / 1000;
+    //   pv.innerHTML = "M Pageviews";
+    // }
   }else{
     plan.innerHTML = "/month";
     pageviews.innerHTML = pageViewsVal[sliderValue];
     price.innerHTML = priceVal[sliderValue];
+    if (pageviews.value >=1000 ) {
+      pageviews.innerHTML = (pageviews.value / 1000);
+      pv.innerHTML = "M Pageviews";
+    }
   }
-  
-  let sliderColor = `linear-gradient(90deg, rgb(165,243,235) ${(sliderValue * 25)}%, rgb(234,238,251) ${(sliderValue * 25)}%)`;
-  range.style.background = sliderColor;
 });
 
+//Switch button
 toggle.addEventListener("click", ()=>{
   toggle.classList.toggle("active");
   circle.classList.toggle("active");
